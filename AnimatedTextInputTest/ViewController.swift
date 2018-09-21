03d15cd799803a5d7e1ba03fc06a6afef3cee696
dqlobo <davidlobosco@gmail.com>
2018-09-21 16:04:59 -0400

    update gig

---
+++ AnimatedTextInputTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedTextInputTest
+//
+//  Created by David Lobosco on 9/21/18.
+//  Copyright © 2018 jobandtalent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
